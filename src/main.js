4a8ff78a2dc35ffda89ac009b4ea42e559937f2e
IRGI_SEPTIANA_SURYA <irgiseptianansurya@gmail.com>
2025-04-13 06:49:53 +0700

    change env

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.post('/auth/login')
+app.post('/auth/register')
+app.get('/user/profile')
+
 app.get('/user', userController.getUser)
 app.post('/user', userController.createUser)
 
