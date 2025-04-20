d6c5365dba122f51ea5a518c0d46684011824963
IRGI_SEPTIANA_SURYA <irgiseptianansurya@gmail.com>
2025-04-20 22:15:22 +0700

    test db railways

--- src/database/db.js
+++ src/database/db.js
@@ -6,7 +6,8 @@ dotenv.config()
 const { Pool } = pkg
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
+  host: process.env.DB_HOST_RAIL,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
