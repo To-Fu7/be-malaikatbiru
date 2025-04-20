d2e16057ec218458900024818255b7bb699271ab
IRGI_SEPTIANA_SURYA <irgiseptianansurya@gmail.com>
2025-04-20 23:06:47 +0700

    change db connection

--- src/database/db.js
+++ src/database/db.js
@@ -5,13 +5,19 @@ import dotenv from 'dotenv'
 dotenv.config()
 const { Pool } = pkg
 
+// const pool = new Pool({
+//   // host: process.env.DB_HOST,
+//   host: process.env.DB_HOST_RAIL,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// })
 const pool = new Pool({
-  // host: process.env.DB_HOST,
-  host: process.env.DB_HOST_RAIL,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export default pool
